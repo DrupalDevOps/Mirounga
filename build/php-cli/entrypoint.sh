72dfce1dd79c72159543be0c5d1ac17c063edce4
rallen-temp <rallen-temp@users.noreply.github.com>
2016-10-18 12:34:42 -0400

    Show user how to CLI debug on PHP-CLI entrypoint.

--- build/php-cli/entrypoint.sh
+++ build/php-cli/entrypoint.sh
@@ -15,5 +15,11 @@ phpcs -i
 echo "Type phpcs --help for more help with PHP Code Sniffer."
 echo ""
 
+# Display some help with debugging.
+echo "[XDebug]"
+echo "To debug any PHP CLI script, use: "
+echo "export PHP_IDE_CONFIG='serverName=docker' \\"
+echo "export XDEBUG_CONFIG='idekey=${XDEBUG_IDE_KEY}'"
+
 # Provide the user with a Z Shell as the entrypoint.
 /bin/zsh --interactive
