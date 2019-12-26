135abfb1fba3451dc5a0e25c778ac396e5e1d123
AlexanderAllen <14018885+AlexanderAllen@users.noreply.github.com>
2019-12-26 06:17:07 -0500

    We nuke APK during. Build, fetch, use, and remove MySQL client manually.
    * The destination paths of these packages using regular APK add are
      indeterminate in the Docker layer, and APK doesn't install to them
      after build.
    * We can fetch the client manually, extract to alternate location, and
      use from there - then remove once install script is done - all
      without adding any additional size to the the built image.

---
+++ build/mariadb-alpine/analyze.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+docker run --rm -it \
+-v /var/run/docker.sock:/var/run/docker.sock \
+wagoodman/dive:latest alexanderallen/mariadb-alpine:10.4.10-r0
+                                                                                                                                                wagoodman/dive:latest alexanderallen/mariadb-alpine:10.4.10-r0
