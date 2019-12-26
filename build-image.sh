605af5349b413a27e2e1c571bb414535c10ca5f7
AlexanderAllen <14018885+AlexanderAllen@users.noreply.github.com>
2019-12-26 06:41:57 -0500

    Initial (new) build script.

---
+++ build-image.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set +x
+
+
+docker-compose -f ./build/nginx/docker-compose.yml build \
+&& docker-compose -f ./build/mariadb-alpine/docker-compose.yml build \
+&& docker-compose -f ./build/php-fpm/docker-compose.yml build \
+&& docker-compose -f ./build/php-cli/docker-compose.yml build
+
+
+#&& docker-compose up -d \
+# && docker-compose ps
