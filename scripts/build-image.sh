9d1bc56b6f5061f90b2f4c4bdcba80b6d85a267b
AlexanderAllen <14018885+AlexanderAllen@users.noreply.github.com>
2020-01-13 16:47:49 -0500

    Add Drupal image to build script.

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -2,12 +2,8 @@
 
 set +x
 
-
 docker-compose -f ./build/nginx/docker-compose.yml build \
 && docker-compose -f ./build/mariadb-alpine/docker-compose.yml build \
 && docker-compose -f ./build/php-fpm/docker-compose.yml build \
-&& docker-compose -f ./build/php-cli/docker-compose.yml build
-
-
-#&& docker-compose up -d \
-# && docker-compose ps
+&& docker-compose -f ./build/php-cli/docker-compose.yml build \
+&& docker-compose -f ./build/drupal/docker-compose.yml build
