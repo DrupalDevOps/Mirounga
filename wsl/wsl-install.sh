c7be88ebe50101903956c09005aa280cfc923f76
AlexanderAllen <14018885+AlexanderAllen@users.noreply.github.com>
2020-01-14 17:07:37 -0500

    Create directory for WSL configuration.

---
+++ wsl/wsl-install.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Script for installing docker client (not daemon) into Windows Subsystem for Linux.
+# Credits: https://nickjanetakis.com/blog/setting-up-docker-for-windows-and-wsl-to-work-flawlessly
+
+# Update the apt package list.
+sudo apt-get update -y
+
+# Install Docker's package dependencies.
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+# Download and add Docker's official public PGP key.
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# Verify the fingerprint.
+sudo apt-key fingerprint 0EBFCD88
+
+# Add the `stable` channel's Docker upstream repository.
+#
+# If you want to live on the edge, you can change "stable" below to "test" or
+# "nightly". I highly recommend sticking with stable!
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+# Update the apt package list (for the new apt repo).
+sudo apt-get update -y
+
+# Install the latest version of Docker CE.
+sudo apt-get install -y docker-ce
+
+# Allow your user to access the Docker CLI without needing root access.
+sudo usermod -aG docker $USER
+
+sudo apt-get install -y python python-pip
+pip install --user docker-compose
+# add pip $HOME/.local/bin to path
