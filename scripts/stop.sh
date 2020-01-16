0358ef6da5a1aa1a46c33946f3c53a079e566896
AlexanderAllen <14018885+AlexanderAllen@users.noreply.github.com>
2020-01-15 19:31:11 -0500

    Add env var to stop script.

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,26 +1,10 @@
 #!/bin/bash
 
-# Using user-defined network eliminates need to use service links,
-# and allows multiple compose stacks to join/leave network on a as-needed basis -
-# as opposed to composing up or down an entire monolith compose stack.
-#
-# For benefits of user-defined bridge over default bridge https://docs.docker.com/network/bridge/
-# Compose networking: https://docs.docker.com/compose/networking/
-# Latest compose reference: https://docs.docker.com/compose/compose-file/#network-configuration-reference
-
-# Create user-defined bridge.
-# USER_NETWORK=localenv
-
-# NETEXISTS=`docker network ls | grep -c $USER_NETWORK`
-# if ! (($NETEXISTS)) ; then
-#   echo "Create user-defined network"
-#   docker network create $USER_NETWORK
-# else
-#   echo "Docker network ${USER_NETWORK} already exists, joining."
-# fi
-
 # Bring down stack if it exists.
+ENV=${ENVIRONMENT:-prod}
+echo "Stopping and removing Docker Compose ${ENV} environment."
+
 docker-compose \
 -f docker-compose.yml \
--f run/drupal/docker-compose.prod.yml \
+-f run/drupal/docker-compose.${ENV}.yml \
 down
