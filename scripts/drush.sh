81c68576ccec34dbca7da19cc005e69d60d507f3
AlexanderAllen <14018885+AlexanderAllen@users.noreply.github.com>
2020-01-28 02:11:56 -0500

    Create separate compose files for drush envionments.

--- scripts/drush.sh
+++ scripts/drush.sh
@@ -2,6 +2,9 @@
 
 # Drush script using production configuration.
 
+ENV=${ENVIRONMENT:-prod}
+echo "Running Drush for ${ENV} environment."
+
 # Start local stack on which to run Drush on.
 ./scripts/start.sh
 
@@ -9,6 +12,6 @@
 # Paths are relative to project root, when invoking script from project root.
 docker-compose \
 -f docker-compose.yml \
--f run/drupal/docker-compose.prod.yml \
--f run/drush/docker-compose.yml \
+-f run/drupal/docker-compose.${ENV}.yml \
+-f run/drush/docker-compose.${ENV}.yml \
 run --rm drush
