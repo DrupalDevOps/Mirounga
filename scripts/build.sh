ea7cde31b9b06ab3914511fe92607c277db70f1f
AlexanderAllen <14018885+AlexanderAllen@users.noreply.github.com>
2020-02-01 08:30:08 -0500

    Build singular service if specified in first argument.

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,9 +2,21 @@
 
 set +x
 
-docker-compose -f ./build/nginx/docker-compose.yml build \
-&& docker-compose -f ./build/xhprof-viewer/docker-compose.yml build
-&& docker-compose -f ./build/mariadb-alpine/docker-compose.yml build \
-&& docker-compose -f ./build/php-fpm/docker-compose.yml build \
-&& docker-compose -f ./build/php-cli/docker-compose.yml build \
-&& docker-compose -f ./build/drupal/docker-compose.yml build
+# Build singular service if specified in first argument.
+# http://tldp.org/LDP/Bash-Beginners-Guide/html/sect_07_01.html
+#
+if [ $1 ]; then
+
+  docker-compose -f ./build/${1}/docker-compose.yml build
+
+else
+
+  docker-compose -f ./build/nginx/docker-compose.yml build \
+  && docker-compose -f ./build/mariadb-alpine/docker-compose.yml build \
+  && docker-compose -f ./build/php-fpm/docker-compose.yml build \
+  && docker-compose -f ./build/php-cli/docker-compose.yml build \
+  && docker-compose -f ./build/drupal/docker-compose.yml build
+
+fi
+
+# && docker-compose -f ./build/xhprof-viewer/docker-compose.yml build \
