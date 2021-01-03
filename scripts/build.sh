b1de42370b7649e89d8a604a823e46ee5257dcd8
AlexanderAllen <14018885+AlexanderAllen@users.noreply.github.com>
2021-01-03 12:46:27 -0500

    Currently not using Drupal source image.

--- scripts/build.sh
+++ scripts/build.sh
@@ -14,8 +14,8 @@ else
   docker-compose -f ./build/nginx/docker-compose.yml build \
   && docker-compose -f ./build/mariadb-alpine/docker-compose.yml build \
   && docker-compose -f ./build/php-fpm/docker-compose.yml build \
-  && docker-compose -f ./build/php-cli/docker-compose.yml build \
-  && docker-compose -f ./build/drupal/docker-compose.yml build
+  && docker-compose -f ./build/php-cli/docker-compose.yml build
+  # && docker-compose -f ./build/drupal/docker-compose.yml build
 
 fi
 
