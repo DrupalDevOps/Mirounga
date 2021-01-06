1efef0a6d5d31f53f67c74423485d9d4d678abc7
AlexanderAllen <14018885+AlexanderAllen@users.noreply.github.com>
2021-01-06 00:06:25 -0500

    Update teardown script.

--- scripts/stop-vsd.sh
+++ scripts/stop-vsd.sh
@@ -4,10 +4,17 @@
 ENV=${ENVIRONMENT:-vsd}
 echo "Stop Docker Compose ${ENV} environment."
 
+LOCALENV_HOME="/home/wsl/Sites/localenv"
+
+# Start shared services.
 docker-compose \
--f docker-compose.yml \
--f run/drupal/docker-compose.vsd.yml \
-down
+--file ${LOCALENV_HOME}/docker-compose.shared.yml down
+
+# Start per-project stack, using current directory as project name.
+# https://stackoverflow.com/a/1371283
+docker-compose \
+--project-name "${PWD##*/}" \
+--file ${LOCALENV_HOME}/run/drupal/docker-compose.vsd.yml down
 
 echo "Remove user bridge network."
 docker network rm VSD
