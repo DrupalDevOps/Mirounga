4c45a430884d4682b1449a651f069a8468c7aa81
AlexanderAllen <14018885+AlexanderAllen@users.noreply.github.com>
2021-01-29 21:06:28 -0500

    Cleanup build.sh: remove proxy container.

--- scripts/build.sh
+++ scripts/build.sh
@@ -14,9 +14,7 @@ else
   docker-compose -f ./build/nginx/docker-compose.yml build \
   && docker-compose -f ./build/mariadb-alpine/docker-compose.yml build \
   && docker-compose -f ./build/php-fpm/docker-compose.yml build \
-  && docker-compose -f ./build/php-cli/docker-compose.yml build \
-  && docker-compose -f ./build/princess/docker-compose.yml build
-  # && docker-compose -f ./build/drupal/docker-compose.yml build
+  && docker-compose -f ./build/php-cli/docker-compose.yml build
 
   docker images | grep alexanderallen
 fi
