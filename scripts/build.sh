22c4eb4c9d28fdec67bf9b45ae88407d00efff9f
AlexanderAllen <14018885+AlexanderAllen@users.noreply.github.com>
2021-01-30 22:13:46 -0500

    New Alpine base image for non-root user.

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,8 +10,8 @@ if [ $1 ]; then
   docker-compose -f ./build/${1}/docker-compose.yml build
 
 else
-
-  docker-compose -f ./build/nginx/docker-compose.yml build \
+  docker-compose -f ./build/nobody/docker-compose.yml build \
+  && docker-compose -f ./build/nginx/docker-compose.yml build \
   && docker-compose -f ./build/mariadb-alpine/docker-compose.yml build \
   && docker-compose -f ./build/php-fpm/docker-compose.yml build \
   && docker-compose -f ./build/php-cli/docker-compose.yml build
