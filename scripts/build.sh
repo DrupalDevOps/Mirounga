675ba67e25bade7c6cd0e107207be2cb5290a8c6
AlexanderAllen <14018885+AlexanderAllen@users.noreply.github.com>
2021-02-01 02:22:51 -0500

    Set and tag base image version via build script.

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,8 +10,15 @@ if [ $1 ]; then
   docker-compose -f ./build/${1}/docker-compose.yml build
 
 else
+  ALPINE_MAJOR=3
+  ALPINE_MINOR=12
+
+  # If there is any base image, build first.
   docker-compose -f ./build/nobody/docker-compose.yml build \
-  && docker-compose -f ./build/varnish/docker-compose.yml build \
+    --no-cache --build-arg ALPINE_MAJOR=$ALPINE_MAJOR --build-arg ALPINE_MINOR=$ALPINE_MINOR
+  docker tag alexanderallen/nobody alexanderallen/nobody:alpine-$ALPINE_MAJOR.$ALPINE_MINOR
+
+  docker-compose -f ./build/varnish/docker-compose.yml build \
   && docker-compose -f ./build/nginx/docker-compose.yml build \
   && docker-compose -f ./build/mariadb-alpine/docker-compose.yml build \
   && docker-compose -f ./build/php-fpm/docker-compose.yml build \
