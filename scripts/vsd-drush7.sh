87ebf255d284afb7ecf12b11a09a2102d2225307
AlexanderAllen <14018885+AlexanderAllen@users.noreply.github.com>
2021-02-16 17:31:21 -0500

    Initial drush7 container.

---
+++ scripts/vsd-drush7.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+export COMPOSE_NETWORK=VSD
+
+export XDEBUG_REMOTE_HOST=`ip addr show eth0 | grep -oE '\d+(\.\d+){3}' | head -n 1`
+echo "XDebug will contact IDE at ${XDEBUG_REMOTE_HOST}"
+
+# Source code directory, assumed to be the current directory.
+export PROJECT_SOURCE=`readlink -f .`
+echo "Project location is ${PROJECT_SOURCE}"
+
+# Must mount source into same location as PHP-FPM and Nginx.
+export PROJECT_DEST="/vsdroot"
+
+PROJECT_NAME="${PWD##*/}"
+LOCALENV_HOME="/home/wsl/Sites/localenv"
+
+# Sharing SSH socket from WSL2 into containers.
+export SSH_AUTH_SOCK=${SSH_AUTH_SOCK}
+
+
+# Start local stack on which to run Drush on.
+# ./scripts/start.sh
+
+# Merge Drush compose onto existing stack definition.
+# Paths are relative to project root, when invoking script from project root.
+docker-compose \
+--project-name $PROJECT_NAME \
+--file ${LOCALENV_HOME}/docker-compose.shared.yml \
+--file ${LOCALENV_HOME}/docker-compose.override.yml \
+--file ${LOCALENV_HOME}/run/drupal/docker-compose.vsd.yml \
+--file ${LOCALENV_HOME}/run/drush/docker-compose.vsd.yml \
+run \
+--entrypoint=ash --rm --user=root \
+drush7
+
+
+
+#run --rm drush "$@"
+# run --rm --user=root drush "$@"
+
+# To connect to database use:
+# mariadb -h mysql
